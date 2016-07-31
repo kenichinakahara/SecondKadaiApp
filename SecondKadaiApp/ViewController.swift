12f6ffd702085f149dfca14d77b066a051311e55
kenic <setsuna@kenimm.local>
2016-08-01 02:35:00 +0900

    2回目の課題です。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        // 遷移先のResultViewControllerで宣言している名前に値を代入して渡す
+        resultViewController.name = name.text!
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+    }
 
 }
 
